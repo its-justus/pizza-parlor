2490d65b70d71d738dd5ccea3dcb279cdb4a2bed
Christopher Neisen <christopherjay71186@gmail.com>
2020-06-18 10:47:36 -0500

    set up index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './components/App/App';
 
 // redux
-import {createStorelse, combineReducers, applyMiddleware} from ''
+import {createStorelse, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
