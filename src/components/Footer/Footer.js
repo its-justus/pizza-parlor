b07e0002618decbb07f9c93297c56e98094ea02d
its-justus <ianjohnson9042@gmail.com>
2020-06-18 22:38:20 -0500

    cleaning up and commenting header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
-let thisDate = new Date();
-let thisYear = thisDate.getFullYear();
-const copyright = "\u00A9";
+
+// Footer is a controlled component that renders the footer of the site
 class Footer extends Component {
+	// React render function
   render() {
+    let thisDate = new Date();
+    let thisYear = thisDate.getFullYear();
+    const copyright = "\u00A9";
     return (
       <footer className="App-footer">
         <p>
           {copyright} Pod 4 {thisYear}
         </p>
       </footer>
-    );
-  }
-}
+    ); // end return
+  } // end render
+} // end class Footer
+
 export default Footer;
